d02029800c768bd1c8261eafb1aa79c0af88beec
JoshuaBudd <34956255+JoshuaBudd@users.noreply.github.com>
2020-09-29 14:26:52 -0400

    Add files via upload

---
+++ CRUD Lab/Form1.Designer.vb
@@ -0,0 +1,251 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.lblPeopleID = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtPeopleID = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.SystemToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PeopleDataSet = New CRUD_Lab.peopleDataSet()
+        Me.PeopleDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnFilter = New System.Windows.Forms.Button()
+        Me.btnSort = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PeopleDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PeopleDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 215)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(517, 290)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(157, 93)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(111, 32)
+        Me.btnUpdate.TabIndex = 1
+        Me.btnUpdate.Text = "Update/Save"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(157, 131)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(83, 32)
+        Me.btnDelete.TabIndex = 5
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(157, 56)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(83, 31)
+        Me.btnAdd.TabIndex = 4
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'lblPeopleID
+        '
+        Me.lblPeopleID.AutoSize = True
+        Me.lblPeopleID.Location = New System.Drawing.Point(94, 30)
+        Me.lblPeopleID.Name = "lblPeopleID"
+        Me.lblPeopleID.Size = New System.Drawing.Size(0, 17)
+        Me.lblPeopleID.TabIndex = 22
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 132)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(76, 17)
+        Me.Label3.TabIndex = 21
+        Me.Label3.Text = "Last Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 17)
+        Me.Label2.TabIndex = 20
+        Me.Label2.Text = "First Name"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 17)
+        Me.Label1.TabIndex = 19
+        Me.Label1.Text = "Persons' ID"
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(12, 152)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(114, 22)
+        Me.txtLastName.TabIndex = 18
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(12, 98)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(114, 22)
+        Me.txtFirstName.TabIndex = 17
+        '
+        'txtPeopleID
+        '
+        Me.txtPeopleID.Location = New System.Drawing.Point(12, 50)
+        Me.txtPeopleID.Name = "txtPeopleID"
+        Me.txtPeopleID.Size = New System.Drawing.Size(114, 22)
+        Me.txtPeopleID.TabIndex = 16
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SystemToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(541, 28)
+        Me.MenuStrip1.TabIndex = 23
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'SystemToolStripMenuItem
+        '
+        Me.SystemToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.SystemToolStripMenuItem.Name = "SystemToolStripMenuItem"
+        Me.SystemToolStripMenuItem.Size = New System.Drawing.Size(68, 24)
+        Me.SystemToolStripMenuItem.Text = "System"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(102, 24)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'PeopleDataSet
+        '
+        Me.PeopleDataSet.DataSetName = "peopleDataSet"
+        Me.PeopleDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'PeopleDataSetBindingSource
+        '
+        Me.PeopleDataSetBindingSource.DataSource = Me.PeopleDataSet
+        Me.PeopleDataSetBindingSource.Position = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(292, 50)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(125, 22)
+        Me.TextBox1.TabIndex = 24
+        '
+        'btnFilter
+        '
+        Me.btnFilter.Location = New System.Drawing.Point(423, 45)
+        Me.btnFilter.Name = "btnFilter"
+        Me.btnFilter.Size = New System.Drawing.Size(83, 32)
+        Me.btnFilter.TabIndex = 25
+        Me.btnFilter.Text = "Filter"
+        Me.btnFilter.UseVisualStyleBackColor = True
+        '
+        'btnSort
+        '
+        Me.btnSort.Location = New System.Drawing.Point(157, 169)
+        Me.btnSort.Name = "btnSort"
+        Me.btnSort.Size = New System.Drawing.Size(83, 32)
+        Me.btnSort.TabIndex = 26
+        Me.btnSort.Text = "Sort"
+        Me.btnSort.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(541, 517)
+        Me.Controls.Add(Me.btnSort)
+        Me.Controls.Add(Me.btnFilter)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.lblPeopleID)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.txtPeopleID)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Joshua Budd CRUD Lab"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PeopleDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PeopleDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents btnUpdate As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents lblPeopleID As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtLastName As System.Windows.Forms.TextBox
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents txtPeopleID As System.Windows.Forms.TextBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents SystemToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PeopleDataSetBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents PeopleDataSet As CRUD_Lab.peopleDataSet
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents btnFilter As System.Windows.Forms.Button
+    Friend WithEvents btnSort As System.Windows.Forms.Button
+
+End Class
---
+++ CRUD Lab/Form1.vb
@@ -0,0 +1,81 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Private Function getConnectionString() As String
+        Return My.Settings.peopleConnectionString
+    End Function
+
+    Private conn As New SqlConnection(getConnectionString())
+    Private da As New SqlDataAdapter("Select * from Peoples", conn)
+    Private ds As New DataSet("PeopleDS")
+    Private dt As New DataTable("Peoples")
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ds.Tables.Add(dt)
+        da.Fill(ds.Tables("Peoples"))
+        DataGridView1.DataSource = ds.Tables("Peoples")
+        Dim commands As New SqlCommandBuilder(da)
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        ds.EndInit()
+        da.Update(ds.Tables("Peoples"))
+    End Sub
+
+    Private Sub AddPeople(fname As String, lname As String, id As String)
+        Dim sql As String
+        Dim conn As SqlConnection
+        Dim cmd As SqlCommand
+        sql = "insert into peoples(firstname, lastname, people_ID) values(@firstname, @lastname, @people_ID)"
+        conn = New SqlConnection(getConnectionString())
+        conn.Open()
+        cmd = New SqlCommand(sql, conn)
+        cmd.Parameters.AddWithValue("@lastname", lname)
+        cmd.Parameters.AddWithValue("@firstname", fname)
+        cmd.Parameters.AddWithValue("@people_ID", id)
+        cmd.ExecuteNonQuery()
+        conn.Close()
+    End Sub
+
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Try
+            AddPeople(txtFirstName.Text, txtLastName.Text, txtPeopleID.Text)
+            MessageBox.Show("Added")
+        Catch ex As Exception
+            MessageBox.Show("Error" & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim sql As String
+        Dim conn As SqlConnection
+        Dim cmd As SqlCommand
+        sql = "delete from peoples where people_ID=@people_ID"
+        conn = New SqlConnection(getConnectionString())
+        conn.Open()
+        cmd = New SqlCommand(sql, conn)
+        cmd.Parameters.AddWithValue("@people_ID", CInt(txtPeopleID.Text))
+        cmd.ExecuteNonQuery()
+        conn.Close()
+        btnUpdate.PerformClick()
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub btnFilter_Click(sender As Object, e As EventArgs) Handles btnFilter.Click
+        Dim dv As New DataView(ds.Tables(0))
+        dv.RowFilter = "people_ID like'%" & TextBox1.Text & "%'"
+        DataGridView1.DataSource = dv
+    End Sub
+
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+        Dim dv As New DataView(ds.Tables(0))
+        DataGridView1.DataSource = dv
+        dv.Sort = "people_ID"
+    End Sub
+End Class
